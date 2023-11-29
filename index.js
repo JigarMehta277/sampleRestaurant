f9f0dd597f9785fb857e11c5edb786ac90e2d393
Jigar Mehta <jigarmehta277@gmail.com>
2023-11-29 15:41:57 -0500

    .

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-//Test code
+const express = require("express");
+const connectDB = require("./config/db");
+const path = require("path");
+
+const app = express();
+
+//Connect Database
+connectDB();
+
+//Init Middleware
+app.use(express.json({ extended: false }));
+
+// Define Routes
+//app.use("/api/", require("./routes/api/"));
+//app.use("/api/", require("./routes/api/"));
+//app.use("/api/", require("./routes/api/"));
+//app.use("/api/", require("./routes/api/"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
